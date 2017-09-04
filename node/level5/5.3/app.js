142b062ecc6e3a31fdc13ab4e717caa7562cc283
Nong Suon <nong.suon@gmail.com>
2017-09-04 03:43:55 +0000

    finished node level 5

---
+++ node/level5/5.3/app.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var app = express();
+
+var quotes = {
+  'einstein': 'Life is like riding a bicycle. To keep your balance you must keep moving',
+  'berners-lee': 'The Web does not just connect machines, it connects people',
+  'crockford': 'The good thing about reinventing the wheel is that you can get a round one',
+  'hofstadter': 'Which statement seems more true: (1) I have a brain. (2) I am a brain.'
+};
+app.get('/quotes/:name', function(req, response) {
+  
+  response.end(quotes[req.params.name]);
+});
+
+
+app.listen(8080);
---
+++ node/level5/5.5/app.js
@@ -0,0 +1,15 @@
+/*global options*/
+var url = require('url');
+
+options = {
+  // add URL options here
+  protocol: "http:",
+  host: 'search.twitter.com',
+  pathname: 'search.json',
+  query: {
+    q: "codeschool"
+  }
+};
+
+var searchURL = url.format(options);
+console.log(searchURL);
