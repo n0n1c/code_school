74c43be7de12bc972e00f97e1d6005a11de7a512
Nong Suon <nong.suon@gmail.com>
2017-09-05 20:39:22 +0000

    finished express level 2

---
+++ express/level2/2.5/client.js
@@ -0,0 +1,13 @@
+/*global $*/
+$(function(){
+
+  $.get('/cities', appendToList); 
+
+  function appendToList(cities) {
+    var list = [];
+    for(var i in cities){
+      list.push($('<li>', { text: cities[i] }));
+    }
+    $('.city-list').append(list);
+  }
+});
