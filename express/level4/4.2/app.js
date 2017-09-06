c1e2029fbaad27ea74603ad04f1008cf018656c9
Nong Suon <nong.suon@gmail.com>
2017-09-06 05:24:40 +0000

    finished express level 4

---
+++ express/level4/4.2/app.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var parseUrlencoded = bodyParser.urlencoded({extended: false});
+app.post('/cities', parseUrlencoded, function (request, response) {
+  var city = createCity(request.body.name, request.body.description);
+  response.status(201).json(city);
+});
+
+app.listen(3000);
+
+var createCity = function(name, description){
+  cities[name] = description;
+  return name; 
+};
